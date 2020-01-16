b1bb85dca46b45dab963a2e6cf79ead72efef40d
Nick <nicholasdahl27@gmail.com>
2020-01-16 15:51:11 -0500

    Added illustrations to About page, fixed background on scroll

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,12 +9,23 @@ export default class Project extends Component {
             backgroundColor: 'rgba(33,37,41,0.9)',
             color: 'oldlace',
             margin: '1em',
-            padding: '1em'
+            padding: '1em',
+            borderRadius: '0.3em'
         }
 
         const projectTitleStyle = {
             fontSize: '1.3em',
             fontFamily: 'bgMed',
+        }
+
+        const categoryStyle = {
+            fontSize: '1.1em'
+        }
+
+        const pStyle = {
+            textAlign: 'left',
+            fontSize: '1.1em',
+            maxWidth: '300px'
         }
 
         const { title, category, thumbnailUrl, altText, description } = this.props.project;
@@ -23,9 +34,9 @@ export default class Project extends Component {
 
             <div style={projectStyle}>
                 <p style={projectTitleStyle}>{title}</p>
-                <p>{category}</p>
+                <p style={categoryStyle}>{category}</p>
                 <img src={thumbnailUrl} alt={altText}/>
-                <p style={{ maxWidth: '300px'}}>{description}</p>
+                <p style={pStyle}>{description}</p>
             </div>
         );
         
