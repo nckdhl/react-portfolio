46c14bbb6fb54a85d4e00659b6298fa54a4b3e33
Nick <nicholasdahl27@gmail.com>
2020-01-18 14:16:19 -0500

    Added site gifs to project cards, added Peter&Pat project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fab, faGithub, faExpeditedssl } from "@fortawesome/free-brands-svg-icons";
+import {
+  fab,
+  faGithub,
+  faExpeditedssl,
+  faChrome
+} from "@fortawesome/free-brands-svg-icons";
 
 export default class Project extends Component {
   render() {
@@ -8,9 +13,9 @@ export default class Project extends Component {
       backgroundColor: "rgba(33,37,41,0.9)",
       color: "oldlace",
       margin: "1em",
-      padding: "1em",
+      padding: "0em 1em 1em 1em",
       borderRadius: "0.3em",
-      height: '100%'
+      height: "100%"
     };
 
     const projectTitleStyle = {
@@ -29,31 +34,80 @@ export default class Project extends Component {
     };
 
     const linkStyle = {
-        color: 'oldlace',
-        position: 'relative',
-        bottom: '5px',
-        left: '5px',
-        textDecoration: 'none'
-    }
+      position: "relative",
+      bottom: "5px",
+      left: "5px"
+    };
+
+    const anchorStyle = {
+      paddingTop: "0.5em",
+      display: "block",
+      color: "oldlace",
+      textDecoration: "none"
+    };
+
+    const tagStyle = {
+      color: "orange",
+      maxWidth: "300px"
+    };
+
+    const imgStyle = {
+      filter: "drop-shadow(0 0 1rem black)"
+    };
 
     const {
       title,
       category,
+      tags,
       thumbnailUrl,
       repoUrl,
+      liveSiteUrl,
       altText,
       description
     } = this.props.project;
+
+    let links = [];
+
+    const gitHubLink = (
+      <a
+        style={anchorStyle}
+        href={repoUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} size="1x" /> GitHub Repo
+      </a>
+    );
+
+    const liveSiteLink = (
+      <a
+        style={anchorStyle}
+        href={liveSiteUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faChrome} size="1x" /> Live Site
+      </a>
+    );
+
+    if (repoUrl && liveSiteUrl) {
+      links = [gitHubLink, liveSiteLink];
+    } else if (repoUrl && !liveSiteUrl) {
+      links = [gitHubLink];
+    } else if (!repoUrl && liveSiteUrl) {
+      links = [liveSiteLink];
+    }
 
     return (
       <div style={projectStyle}>
         <p style={projectTitleStyle}>{title}</p>
         <p style={categoryStyle}>{category}</p>
-        <img src={thumbnailUrl} alt={altText} />
+        <p style={tagStyle}>{tags}</p>
+        <img style={imgStyle} src={thumbnailUrl} alt={altText} />
         <p style={pStyle}>{description}</p>
-        <a style={linkStyle} href={repoUrl} target="_blank">
-          <FontAwesomeIcon style={{ marginBottom: '-4px'}} icon={faGithub} size="2x" /> GitHub repository
-        </a>
+        <div style={linkStyle}>
+          { links }
+        </div>
       </div>
     );
   }
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,63 +1,78 @@
-import React, { Component } from 'react';
-import Project from './Project';
+import React, { Component } from "react";
+import Project from "./Project";
 
 export default class ProjectContainer extends Component {
-    constructor (props) {
-        super(props);
-    
-        this.state = {
-            projects: [
-                {
-                    title: 'BUBBLY MAMMOTH', 
-                    category: 'Business Website',
-                    thumbnailUrl: require('../assets/images/wireframes-watercolour-resized.jpg'),
-                    repoUrl: 'https://github.com/nckdhl/bubblymammoth-netlify',
-                    altText: 'Website watercolour wireframe',
-                    description: 'Bubbly Mammoth is a business I run together with my wife. ' +
-                                 'We sell handmade bubble toys'
-                },
-                {
-                    title: 'IN-BROWSER VOIP CLIENT', 
-                    category: 'VOIP Javascript Dialer',
-                    thumbnailUrl: require('../assets/images/resized-vanilla-animation3.gif'),
-                    repoUrl: 'https://github.com/nckdhl/vanilla-twilio-client',
-                    altText: 'Website watercolour wireframe',
-                    description: 'This project is essentially a dialpad to make outgoing voice calls. ' +
-                                 'It uses the Twilio programmable voice API. ' +
-                                 'The back-end is a python Flask app. The front-end is React.'
-                },
-                {
-                    title: 'FOCUS FARM TO-DO APP', 
-                    category: 'Vanilla JS and PHP Webapp',
-                    thumbnailUrl: require('../assets/images/wireframes-watercolour-resized.jpg'),
-                    repoUrl: '#',
-                    altText: 'Website watercolour wireframe',
-                    description: 'This was a school project for my PHP and JS course. ' +
-                                 'It is a basic to-do list app.'
-                }
-            ]
-        };
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-
-        const containerStyles = {
-            display: 'flex',
-            justifyContent: 'center',
-            flexFlow: 'row wrap'
+    this.state = {
+      projects: [
+        {
+          title: "BUBBLY MAMMOTH",
+          category: "Business Website",
+          tags: "#StaticHTML #Netlify",
+          thumbnailUrl: require("../assets/images/bubblymammoth-300px.gif"),
+          repoUrl: "https://github.com/nckdhl/bubblymammoth-netlify",
+          liveSiteUrl: "https://bubblymammoth.com",
+          altText: "animated Bubbly Mammoth website",
+          description:
+            "Bubbly Mammoth is a business I run together with my wife. " +
+            "This website is just static HTML and CSS, deployed to Netlify. " +
+            "It is the Canadian arm of Peter&Pat Seifenblasen that we are currently working to launch in 2020. "
+        },
+        {
+          title: "IN-BROWSER VOIP CLIENT",
+          category: "Fullstack Javascript Web App",
+          tags: "#NodeJS #TwilioAPI #Heroku #Netlify #VanillaJS #postgreSQL",
+          thumbnailUrl: require("../assets/images/vanillavoip-300px.gif"),
+          repoUrl: "https://github.com/nckdhl/vanilla-twilio-client",
+          liveSiteUrl: "https://vanillavoip.com",
+          altText: "animated Vanilla VOIP website",
+          description:
+            "This app is essentially a VOIP client to make and receive voice calls. " +
+            "It uses the Twilio programmable voice API and a Twilio phone number. " +
+            "The back-end is Node.js Express deployed to Heroku. The front-end is vanilla JS deployed to Netlify."
+        },
+        {
+          title: "FOCUS FARM TO-DO APP",
+          category: "Vanilla JS and PHP Web App",
+          tags: "#PHP #VanillaJS #MySQL #Bootstrap",
+          thumbnailUrl: require("../assets/images/focusfarm-300px.gif"),
+          repoUrl: "https://github.com/nckdhl/focusfarm-todolist",
+          liveSiteUrl: "",
+          altText: "animated Focus Farm website",
+          description:
+            "This was a school project for my PHP and JS course. " +
+            "It is a basic to-do list app."
+        },
+        {
+          title: "PETER&PAT SEIFENBLASEN",
+          category: "Business Website",
+          tags: "#Wordpress #Woocommerce #Divi",
+          thumbnailUrl: require("../assets/images/peterandpat-300px.gif"),
+          repoUrl: "",
+          liveSiteUrl: "https://peter-pat-seifenblasen.de",
+          altText: "animated Peter & Pat Seifenblasen website",
+          description:
+            "This is the website for an online store I run with my wife and brother-in-law in Germany. " +
+            "We sell giant bubble toys in German-speaking Europe. It is a Wordpress/Woocommerce site that I overhauled in 2016 and maintain part-time. "
         }
-        
-        let projects = this.state.projects.map(project => {
-            console.log(project);
-            return (
-                <Project key={project.title} project={project} />
-            );
-        })
-        
-        return (
-            <div style={containerStyles}>
-                { projects }
-            </div>
-        );
-    }
+      ]
+    };
+  }
+
+  render() {
+    const containerStyles = {
+      display: "flex",
+      justifyContent: "center",
+      flexFlow: "row wrap"
+    };
+
+    let projects = this.state.projects.map(project => {
+      console.log(project);
+      return <Project key={project.title} project={project} />;
+    });
+
+    return <div style={containerStyles}>{projects}</div>;
+  }
 }
