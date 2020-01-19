b5d1b37d68fb61e5f9b8702cfe44d35c6a91b9b7
Nick <nicholasdahl27@gmail.com>
2020-01-19 14:07:17 -0500

    Changed form handling to match syntax from Netlify docs

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,16 +2,43 @@ import React, { Component } from "react";
 import resume from "../assets/images/resume.pdf";
 import("./css/Form.css");
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
 export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
   render() {
+    const { name, email, message } = this.state;
     return (
       <div className="fadein" id="formContainer">
-        <form name="contact" method="POST" data-netlify="true">
-          <input placeholder="NAME" type="text" name="name" required />
+        <form name="contact" method="POST" data-netlify="true" onSubmit={this.handleSubmit}>
+          <input placeholder="NAME" type="text" name="name" value={name} onChange={this.handleChange} required />
 
-          <input placeholder="EMAIL" type="email" name="email" required />
+          <input placeholder="EMAIL" type="email" name="email" value={email} onChange={this.handleChange} required />
 
-          <textarea placeholder="MESSAGE" type="text" name="message"></textarea>
+          <textarea placeholder="MESSAGE" type="text" name="message" value={message} onChange={this.handleChange} required ></textarea>
 
           <button type="submit">SEND</button>
         </form>
